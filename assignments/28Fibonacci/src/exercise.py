519f9818584b794c687e22532862c613b783822e
Zitlalliml <89087259+Zitlalliml@users.noreply.github.com>
2021-09-07 17:22:28 -0500

    Update exercise.py

--- assignments/28Fibonacci/src/exercise.py
+++ assignments/28Fibonacci/src/exercise.py
@@ -1,6 +1,16 @@
 
 def main():
     #escribe tu código abajo de esta línea
+    num = int (input('Enter a number:'))
+    a= 0
+    b= 1
+    for i in range (1,num):
+        c = a + b
+        a = b
+        b = c
+
+    
+print (b, end=" ")
     pass
 
 if __name__=='__main__':
