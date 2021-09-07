1d7e8b430c78ddaa7642a54d32dd705ea36fc18a
Zitlalliml <89087259+Zitlalliml@users.noreply.github.com>
2021-09-07 13:00:39 -0500

    Update exercise.py

--- assignments/17NCuadradoMayor/src/exercise.py
+++ assignments/17NCuadradoMayor/src/exercise.py
@@ -2,6 +2,12 @@
 
 def main():
     #Escribe tu código debajo de esta línea
+    num = int(input("Escribe un numero : "))
+    for j in range(num):
+        p = j **2
+        if p > num:
+            print (j)
+            break
     pass
 
 if __name__=='__main__':
