2df371d2b7aa18efe8625dc73b9cb594a40f8fcb
Zitlalliml <89087259+Zitlalliml@users.noreply.github.com>
2021-09-07 11:06:33 -0500

    Update exercise.py

--- assignments/25TrianguloAsteriscos/src/exercise.py
+++ assignments/25TrianguloAsteriscos/src/exercise.py
@@ -1,6 +1,12 @@
 
 def main():
     #Escribe tu código debajo de esta línea
+    height = int(input("Enter triangle height: "))
+
+    for i in range(height):
+        for j in range(i + 1):
+            print("* ", end="")
+        print()
     pass
 
 
